d1cdd50804e1c8dddc0b2399858fdb0ac03534c8
Nils Sanderson <thebaldwan@gmail.com>
2017-05-18 16:13:19 +0100

    react-redux and firebase minimum boilerplate setup

---
+++ App/App.js
@@ -0,0 +1,38 @@
+
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './Reducers';
+
+
+class App extends Component {
+    componentWillMount() {
+        const config = {
+            apiKey: 'AIzaSyA5XQJA5kSgnqJ1F6LGqkTF9EDHLxV_a18',
+            authDomain: 'manager-a41be.firebaseapp.com',
+            databaseURL: 'https://manager-a41be.firebaseio.com',
+            projectId: 'manager-a41be',
+            storageBucket: 'manager-a41be.appspot.com',
+            messagingSenderId: '741606945576'
+        };
+
+        firebase.initializeApp(config);
+    }
+
+    render() {
+        return (
+            <Provider store={createStore(reducers)}>
+                <View>
+                    <Text>
+                        Hello
+                    </Text>
+                </View>
+            </Provider>
+        );
+    }
+}
+
+
+export default App;
